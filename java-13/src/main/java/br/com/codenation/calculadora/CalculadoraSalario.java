f6234461f0a6a0e4f933f6c26814e8bf4cf63ba1
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-05-01 12:23:46 -0300

    Apply a minimum of oo

--- java-13/src/main/java/br/com/codenation/calculadora/CalculadoraSalario.java
+++ java-13/src/main/java/br/com/codenation/calculadora/CalculadoraSalario.java
@@ -6,35 +6,52 @@ public class CalculadoraSalario {
 		// Use o Math.round apenas no final do método para arredondar o valor final.
 		// Documentação do método:
 		// https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html#round-double-
-		double descontadoInss = salarioBase - descontoInss(salarioBase);
-		double descontadoIrrf = descontadoInss - descontoIrrf(descontadoInss);
-		return descontadoIrrf >= 1039.0 ? Math.round(descontadoIrrf) : 0;
+		// double descontadoInss = salarioBase - descontoInss(salarioBase);
+		// double descontadoIrrf = descontadoInss - descontoIrrf(descontadoInss);
+		// return descontadoIrrf >= 1039.0 ? Math.round(descontadoIrrf) : 0;
+		return new SalarioLiquido(salarioBase).valor();
 	}
 
-	// Exemplo de método que pode ser criado para separar melhor as
-	// responsábilidades de seu algorítmo
-	private double descontoInss(double salarioBase) {
-		return salarioBase * taxaInss(salarioBase);
+}
+
+class SalarioLiquido {
+	private double salarioBase;
+	private static final double SALARIO_MINIMO = 1039.0;
+
+	public SalarioLiquido(double salarioBase) {
+		this.salarioBase = salarioBase;
 	}
 
-	private double taxaInss(double salarioBase) {
-		if (salarioBase <= 1500)
-			return 0.08;
-		if (salarioBase <= 4000)
-			return 0.09;
-		return 0.11;
+	public long valor() {
+		double valor = this.descontadoIrrf();
+		if (valor < SalarioLiquido.SALARIO_MINIMO)
+			return 0L;
+		return Math.round(valor);
 	}
 
-	private double descontoIrrf(double salarioDescontado) {
-		return salarioDescontado * taxaIrrf(salarioDescontado);
+	private double descontadoIrrf() {
+		return this.descontadoInss() * (1 - this.taxaIrrf());
 	}
 
-	private double taxaIrrf(double salarioDescontado) {
-		if (salarioDescontado <= 3000)
+	private double taxaIrrf() {
+		double descontado = this.descontadoInss();
+		if (descontado <= 3000.0)
 			return 0.0;
-		if (salarioDescontado <= 6000)
+		if (descontado <= 6000.0)
 			return 0.075;
 		return 0.15;
+	}
+
+	private double descontadoInss() {
+		return this.salarioBase * (1 - this.taxaInss());
+	}
+
+	private double taxaInss() {
+		if (this.salarioBase <= 1500.0)
+			return 0.08;
+		if (this.salarioBase <= 4000.0)
+			return 0.09;
+		return 0.11;
 	}
 
 }
