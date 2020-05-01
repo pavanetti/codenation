5b5cb9afcb87bd2970afc961c82179cb205fb935
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-30 23:36:59 -0300

    Update resolution with complete answer

--- java-0/src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
+++ java-0/src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
@@ -6,11 +6,20 @@ import java.util.List;
 public class DesafioApplication {
 
 	public static List<Integer> fibonacci() {
-		return new ArrayList<Integer>();
+		ArrayList<Integer> sequence = new ArrayList<>(List.of(0, 1));
+
+		int last, penultimate;
+		do {
+			last = sequence.get(sequence.size() - 1);
+			penultimate = sequence.get(sequence.size() - 2);
+			sequence.add(last + penultimate);
+		} while (last + penultimate < 350);
+
+		return sequence;
 	}
 
 	public static Boolean isFibonacci(Integer a) {
-		return true;
+		return fibonacci().contains(a);
 	}
 
 }
