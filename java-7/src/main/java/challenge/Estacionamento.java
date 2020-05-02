82ee9b7f02f0f8632e8c8cd12c4c1d4201717010
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-05-02 11:42:04 -0300

    Pass driver parking tests

--- java-7/src/main/java/challenge/Estacionamento.java
+++ java-7/src/main/java/challenge/Estacionamento.java
@@ -3,7 +3,7 @@ package challenge;
 public class Estacionamento {
 
     public void estacionar(Carro carro) {
-
+        validarMotorista(carro.getMotorista());
     }
 
     public int carrosEstacionados() {
@@ -12,5 +12,14 @@ public class Estacionamento {
 
     public boolean carroEstacionado(Carro carro) {
         return true;
+    }
+
+    private void validarMotorista(Motorista motorista) {
+        if (motorista == null)
+            throw new EstacionamentoException("Não pode ter carro autônomo");
+        if (motorista.getIdade() < 18)
+            throw new EstacionamentoException("Não pode ter motorista menor de idade");
+        if (motorista.getPontos() > 20)
+            throw new EstacionamentoException("Não pode ter motorista com habilitação suspensa");
     }
 }
