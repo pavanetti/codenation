befd953f69a5c4e29cafca9179351c362a78dcfe
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-05-02 11:01:29 -0300

    Pass driver age test

--- java-7/src/main/java/challenge/Motorista.java
+++ java-7/src/main/java/challenge/Motorista.java
@@ -83,6 +83,8 @@ public class Motorista {
         }
 
         public MotoristaBuilder withIdade(int idade) {
+            if (idade < 0)
+                throw new IllegalArgumentException();
             this.idade = idade;
             return this;
         }
