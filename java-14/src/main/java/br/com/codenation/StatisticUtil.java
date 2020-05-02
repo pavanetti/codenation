a1fe9e5d497d0cb17e58d775409b65738ba3fd1d
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-05-02 14:21:03 -0300

    Implement mode

--- java-14/src/main/java/br/com/codenation/StatisticUtil.java
+++ java-14/src/main/java/br/com/codenation/StatisticUtil.java
@@ -1,16 +1,20 @@
 package br.com.codenation;
 
 import java.util.Arrays;
-import java.util.stream.DoubleStream;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StatisticUtil {
 	public static int average(int[] elements) {
-		DoubleStream stream = Arrays.stream(elements).mapToDouble(Double::valueOf);
-		return (int) stream.average().orElse(Double.NaN);
+		return (int) Arrays.stream(elements).average().orElse(Double.NaN);
 	}
 
 	public static int mode(int[] elements) {
-		return 3;
+		Stream<Integer> stream = Arrays.stream(elements).boxed();
+		Map<Integer, Long> counting = stream.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		return counting.entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse(0);
 	}
 
 	public static int median(int[] elements) {
