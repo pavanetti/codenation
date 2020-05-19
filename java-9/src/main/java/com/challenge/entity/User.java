5d9b27bd60f5e3eddfcf7a8a9a7ac14ff969237e
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-05-19 20:22:24 -0300

    Pass user own specs

--- java-9/src/main/java/com/challenge/entity/User.java
+++ java-9/src/main/java/com/challenge/entity/User.java
@@ -1,12 +1,35 @@
 package com.challenge.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Entity
 public class User {
+    @OneToMany
+    private List<Submission> submissions;
+
+    @OneToMany
+    private List<Candidate> candidates;
+
     @NotNull
     @Size(max = 100)
     private String fullname;
+
+    @Email
+    @NotNull
+    @Size(max = 100)
+    private String email;
+
+
+    @NotNull
+    @Size(max = 50)
+    private String nickname;
+
+    @NotNull
+    @Size(max = 255)
+    private String password;
 }
