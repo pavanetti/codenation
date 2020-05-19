5abc5fcf160028d0a4f5518440613b4173064fb5
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-05-19 20:11:42 -0300

    Complete ser fullname specs

--- java-9/src/main/java/com/challenge/entity/User.java
+++ java-9/src/main/java/com/challenge/entity/User.java
@@ -1,8 +1,12 @@
 package com.challenge.entity;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class User {
+    @NotNull
+    @Size(max = 100)
     private String fullname;
 }
