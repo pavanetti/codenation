938dd607354c828733176976b119b4a68fa69890
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-05-19 20:34:08 -0300

    Pass inherited specs

--- java-9/src/main/java/com/challenge/entity/User.java
+++ java-9/src/main/java/com/challenge/entity/User.java
@@ -1,19 +1,30 @@
 package com.challenge.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Date;
 import java.util.List;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class User {
     @OneToMany
     private List<Submission> submissions;
 
     @OneToMany
     private List<Candidate> candidates;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private Date createdAt;
 
     @NotNull
     @Size(max = 100)
